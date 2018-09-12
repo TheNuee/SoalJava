731d6b962143b1af9f70758b416e312e0fa62954
Rizky <Rizky@DESKTOP-HV8NOO1>
2018-09-12 19:28:05 +0700

    initial commit

--- src/soaljava1/SoalJava1.java
+++ src/soaljava1/SoalJava1.java
@@ -6,11 +6,27 @@ public class SoalJava1 {
 
     public static void main(String[] args) {
         // Atribut
-        int nm, hrg, kd, jml, ttl, Bayar = 0, ulangi, diskon = 0;
+        int nm, hrg, kd, jml, ttl, Bayar = 0, ulangi, diskon = 0, ulg;
+        
+        boolean a = true;
         Scanner Scanner = new Scanner(System.in);
-        do {
+        
             System.out.println("Selamat Datang Di Setya ATK Store");
             System.out.println("==================================");
+            System.out.print(" Nama Kasir : ");
+            String namaKasir = Scanner.next().toUpperCase();
+            if (namaKasir == "ADMIN") {
+                ulg = 1;
+                a = false;
+            } else {
+                ulg = 0;
+            }
+            if (ulg == 0) {
+                System.out.print(" Nama Kasir : ");
+                namaKasir = Scanner.next().toLowerCase();
+            }
+            System.out.println("==================================");
+            do{
             System.out.print(" Nama Barang : ");
             String namaBarang = Scanner.next();
             System.out.print(" Masukkan Jumlah : ");
@@ -21,13 +37,16 @@ public class SoalJava1 {
             if (ttl > 100000) {
                 diskon = (int) (0.10 * ttl);
             }
-            if (ttl >= 200000) {
+            else if (ttl >= 200000) {
                 diskon = (int) (0.20 * ttl);
             }
-            if (ttl >= 500000) {
+            else if (ttl >= 500000){
                 diskon = (int) (0.50 * ttl);
 
+            }if (ttl>=100000) {
                 Bayar = ttl - diskon;
+            }else{
+                Bayar=ttl;
             }
             System.out.println("Total Bayar = Rp" + Bayar);
 
